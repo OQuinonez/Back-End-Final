410f98e4e3187fb5d5f610f054a984e579914879
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2018-03-20 16:22:51 -0500

    Created the Product Class

---
+++ backend/src/main/java/com/example/backend/Product.java
@@ -0,0 +1,23 @@
+package com.example.backend;
+
+public class Product {
+    public String ItemName;
+    public String Category;
+    public Double Price;
+    public Integer Quantity;
+    public String PicAddress;
+
+
+    public Product(String ItemName, String Category, Double Price, Integer Quantity, String PicAddress){
+        this.ItemName = ItemName;
+        this.Category = Category;
+        this.Price = Price;
+        this.Quantity = Quantity;
+        this.PicAddress = PicAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "Item Name(" + ItemName + ")";
+    }
+}
