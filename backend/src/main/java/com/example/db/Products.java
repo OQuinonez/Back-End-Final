8dd4a9c16bbb32e5992949b9c65d74b66100aa64
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2018-03-20 16:21:57 -0500

    Connected the file to the DB

--- backend/src/main/java/com/example/db/Products.java
+++ backend/src/main/java/com/example/db/Products.java
@@ -1,10 +1,14 @@
 package com.example.db;
 
+import com.example.backend.Connect;
+
+import java.sql.Connection;
 import java.util.List;
 
 public class Products {
     public static List<Products> all() {
         //Connect
+        Connection con = Connect.LoadDB();
         //SELECT * FROM PRODUCTS
          return fetchall();
     }
