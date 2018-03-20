3088ab57dcb746a8738e7790c7ba4e75848c49f5
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2018-03-20 11:15:15 -0500

    Created the Product Controller

---
+++ backend/src/main/java/com/example/backend/ProductController.java
@@ -0,0 +1,27 @@
+package com.example.backend;
+
+
+import com.example.db.Products;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+    @CrossOrigin();
+
+    @GetMapping("/products")
+
+    public List<Product> all() {
+        return Products.all();
+    }
+
+    @CrossOrigin();
+
+    @PostMapping("/products/new")
+    public Products newProduct(@RequestParam Products product){
+        Products.create(product);
+        return product;
+    }
+
+}
