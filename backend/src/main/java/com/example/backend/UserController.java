6b6038a0054c11d53299adc1a0f468a34e90f29b
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2018-03-20 08:37:18 -0500

    First Commit, Started writing the methods I am going to have for the UserController

---
+++ backend/src/main/java/com/example/backend/UserController.java
@@ -0,0 +1,14 @@
+package com.example.backend;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserController {
+    public static void DisplayUserInfo(Integer UserID){
+        try {
+            Connection con = Connect.LoadDB();
+        } catch ()
+    }
+}
