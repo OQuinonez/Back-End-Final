5999af23300f11798b30fd6e0b2ba3133bde1f29
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2018-03-30 08:53:13 -0500

    Added a method that will give me all of the users

--- backend/src/main/java/com/example/backend/UserRepository.java
+++ backend/src/main/java/com/example/backend/UserRepository.java
@@ -17,10 +17,11 @@ public class UserRepository {
             ArrayList<User> allUsers = new ArrayList<User>();
             while (resultSet.next()){
                 allUsers.add(new
-                        User(resultSet.getString("UserName"),
+                        User(resultSet.getInt("UserId"), resultSet.getString("UserName"),
                         resultSet.getString("UAddress"),
                         resultSet.getString("Email"),
-                        resultSet.getString("HashCode")));
+                        resultSet.getString("HashCode"),
+                        resultSet.getString("session_key")));
             }
             con.close();
             return allUsers;
@@ -30,23 +31,20 @@ public class UserRepository {
         }
     }
 
-    public static User CreatingUser(String UserName, String UAddress, String Email, String Password){
+    public static User CreatingUser(String UserName, String UAddress, String Email, String pass_word, String session_key){
         try {
             Connection con = Connect.LoadDB();
             PreparedStatement statement = con.prepareStatement(
-                    "INSERT INTO Users (" +
-                            "UserName, UAddress, Email, Password)" +
-                            "VALUES (?, ?, ?, ?)");
+                    "INSERT INTO Users (UserName, UAddress, Email, pass_word, sessionKey) VALUES (?, ?, ?, ?, ?) RETURNING *");
             statement.setString(1, UserName);
             statement.setString(2,UAddress);
             statement.setString(3,Email);
-            statement.setString(4,Password);
-
-            ResultSet resultSet = statement.executeQuery();
-            resultSet.next();
-            con.close();
-            return new User(UserName,
-                    UAddress, Email, Password);
+            statement.setString(4, pass_word);
+            statement.setString(5, session_key);
+            ResultSet result = statement.executeQuery();
+            result.next();
+            return new User(result.getInt("UserID"), result.getString("UserName"),
+                    result.getString("UAddress"), result.getString("Email"), result.getString("pass_word"), result.getString("sessionkey"));
         }
         catch (SQLException e) {
             System.out.println(e.getMessage());
@@ -54,42 +52,85 @@ public class UserRepository {
         }
     }
 
-    public static User isUser(String Email, String Password){
+    public static ArrayList<User> allUsers() {
+        try {
+            Connection con = Connect.LoadDB();
+            PreparedStatement statement = con.prepareStatement("SELECT * FROM Users");
+            ResultSet resultSet = statement.executeQuery();
+            ArrayList<User> users = new ArrayList<User>();
+            while (resultSet.next()) {
+                allUsers().add(new User(resultSet.getInt("UserId"),
+                        resultSet.getString("UserName"),
+                        resultSet.getString("UAddress"),
+                        resultSet.getString("Email"),
+                        resultSet.getString("pass_word"),
+                        resultSet.getString("session_key")));
+            }
+            con.close();
+            System.out.println(allUsers().get(1).UserName);
+            return allUsers();
+        } catch (SQLException e) {
+            return null;
+        }
+    }
+
+    public static User isUser(String Email, String pass_word){
         try {
             Connection con = Connect.LoadDB();
             PreparedStatement statement = con.prepareStatement(
-                    "UPDATE Users WHERE Email = ? and password = ? returning *"
+                    "UPDATE Users WHERE Email = ? and pass_word = ? returning *"
             );
             statement.setString(1,Email);
-            statement.setString(2,Password);
+            statement.setString(2,pass_word);
             ResultSet resultSet = statement.executeQuery();
             resultSet.next();
             con.close();
-            return new User(resultSet.getString("UserName"),
+            return new User(resultSet.getInt("UserId"),resultSet.getString("UserName"),
                     resultSet.getString("UAddress"),
                     resultSet.getString("Email"),
-                    resultSet.getString("HashCode"));
+                    resultSet.getString("HashCode"),
+                    resultSet.getString("session_key"));
         }
         catch (SQLException e){
             return null;
         }
     }
 
-    public static boolean deleteUser(String Email, String Password){
+    public static User existingUser(String email, String password, String session_key) {
         try {
             Connection con = Connect.LoadDB();
-            PreparedStatement preparedStatement = con.prepareStatement(
-                    "DELETE FROM Users WHERE id = ?"
-            );
-            preparedStatement.setString(1, Email);
-            preparedStatement.setString(2,Password);
-            preparedStatement.execute();
+            PreparedStatement statement = con.prepareStatement("UPDATE student SET session_key = ? WHERE email = ? and p_word = ? RETURNING *");
+            statement.setString(1, session_key);
+            statement.setString(2, email);
+            statement.setString(3, password);
+            System.out.println(email);
+            System.out.println(password);
+            ResultSet resultSet = statement.executeQuery();
+            resultSet.next();
             con.close();
-            return true;
-        }
-        catch (SQLException e){
+            resultSet.getString(email);
+            resultSet.getString(password);
+        } catch (SQLException e){
             System.out.println(e.getMessage());
-            return false;
+            return null;
         }
     }
+
+//    public static boolean deleteUser(String Email, String Password){
+//        try {
+//            Connection con = Connect.LoadDB();
+//            PreparedStatement preparedStatement = con.prepareStatement(
+//                    "DELETE FROM Users WHERE id = ?"
+//            );
+//            preparedStatement.setString(1, Email);
+//            preparedStatement.setString(2,Password);
+//            preparedStatement.execute();
+//            con.close();
+//            return true;
+//        }
+//        catch (SQLException e){
+//            System.out.println(e.getMessage());
+//            return false;
+//        }
+//    }
 }
