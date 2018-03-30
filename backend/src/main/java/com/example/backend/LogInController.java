065aeba6dc05e51d42800830bcfcff01299abfea
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2018-03-30 08:44:45 -0500

    Created the LogInController which helps me convert the password into a hashcode

---
+++ backend/src/main/java/com/example/backend/LogInController.java
@@ -0,0 +1,35 @@
+package com.example.backend;
+
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Random;
+
+@RestController
+public class LogInController {
+    @Value("${app.salt}")
+    private String salt;
+    @CrossOrigin(allowedHeaders="*",allowCredentials="true")
+    @PostMapping("/LogIn")
+    public User logIn(@RequestParam SignUp newUser){
+        String hashedPassword = BCrypt.hashpw(newUser.pass_word, salt);
+        String alphabet= "abcdefghijklmnopqrstuvwxyz";
+        String sessionKey = "";
+        Random random = new Random();
+        int randomLen = 12+random.nextInt(9);
+        for (int i = 0; i < randomLen; i++) {
+            char c = alphabet.charAt(random.nextInt(26));
+            sessionKey+=c;
+        }
+        User newMember = UserRepository.CreatingUser(newUser.UserName,
+                newUser.UAddress, newUser.Email,
+                hashedPassword, sessionKey);
+        if(newMember != null){
+            return newMember;
+        } else {
+            System.out.println("There was an error");
+            return null;
+        }
+    }
+}
