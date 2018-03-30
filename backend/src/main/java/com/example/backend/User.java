88835f1aea1e90208d8eafa7651f1fe22470516c
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2018-03-30 08:52:12 -0500

    Added a UserId to the User, and a session_key

--- backend/src/main/java/com/example/backend/User.java
+++ backend/src/main/java/com/example/backend/User.java
@@ -1,15 +1,19 @@
 package com.example.backend;
 
 public class User {
+    public Integer UserId;
     public String UserName;
     public String UAddress;
     public String Email;
-    public String HashCode;
-
-    public User(String UserName, String UAddress, String Email, String HashCode){
+    public String pass_word;
+    public String session_key;
+    public User() {}
+    public User(Integer UserId, String UserName, String UAddress, String Email, String pass_word, String session_key){
+        this.UserId = UserId;
         this.UserName = UserName;
         this.UAddress = UAddress;
         this.Email = Email;
-        this.HashCode = HashCode;
+        this.pass_word = pass_word;
+        this.session_key = session_key;
     }
 }
