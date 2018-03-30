54a4a0ddf00650fb3c075e6d998b88b930ef35f6
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2018-03-30 08:51:34 -0500

    Created the SignUp Controler which takes me to the webPage, and helps me convert the password into a HashCode

---
+++ backend/src/main/java/com/example/backend/SignUpController.java
@@ -0,0 +1,34 @@
+package com.example.backend;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.origin.SystemEnvironmentOrigin;
+import org.springframework.web.bind.annotation.*;
+import org.mindrot.jbcrypt.BCrypt;
+
+import java.util.Random;
+
+@RestController
+public class SignUpController {
+    @Value("${app.salt}")
+    private String salt;
+    @CrossOrigin
+    @PostMapping("/SignUp")
+    public User signUp(@RequestBody SignUp newUser){
+        System.out.println(newUser.UserName);
+        System.out.println(newUser.Email);
+        System.out.println(newUser.pass_word);
+        System.out.println(newUser.UAddress);
+
+        String hashedPassword = BCrypt.hashpw(newUser.pass_word, salt);
+        String alphabet= "abcdefghijklmnopqrstuvwxyz";
+        String sessionKey = "";
+        Random random = new Random();
+        int randomLen = 12+random.nextInt(9);
+        for (int i = 0; i < randomLen; i++) {
+            char c = alphabet.charAt(random.nextInt(26));
+            sessionKey+=c;
+        }
+        System.out.println("Session Key: " + sessionKey);
+        return UserRepository.CreatingUser(newUser.UserName,  newUser.UAddress,  newUser.Email, hashedPassword, sessionKey);
+    }
+}
