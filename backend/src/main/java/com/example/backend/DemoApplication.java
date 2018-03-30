5ad1122b0d5a83c23f5b1b65dc86797dc7e64486
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2018-03-30 08:41:33 -0500

    Printed out Bcrypt.gensalt() so I could use it for the app.salt

--- backend/src/main/java/com/example/backend/DemoApplication.java
+++ backend/src/main/java/com/example/backend/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.backend;
 
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +9,6 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		//System.out.println(BCrypt.gensalt());
 	}
 }
