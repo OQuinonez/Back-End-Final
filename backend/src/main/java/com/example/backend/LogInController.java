f92ca1d096513788e73588b7716d89368be07a9a
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2018-04-10 15:31:25 -0500

    Modified the function to where I would actually be able to log in

--- backend/src/main/java/com/example/backend/LogInController.java
+++ backend/src/main/java/com/example/backend/LogInController.java
@@ -4,30 +4,28 @@ import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.SQLException;
 import java.util.Random;
 
 @RestController
 public class LogInController {
     @Value("${app.salt}")
     private String salt;
-    @CrossOrigin(allowedHeaders="*",allowCredentials="true")
-    @PostMapping("/LogIn/")
-    public User logIn(@RequestParam SignUp newUser){
-        String hashedPassword = BCrypt.hashpw(newUser.pass_word, salt);
-        String alphabet= "abcdefghijklmnopqrstuvwxyz";
-        String sessionKey = "";
-        Random random = new Random();
-        int randomLen = 12+random.nextInt(9);
-        for (int i = 0; i < randomLen; i++) {
-            char c = alphabet.charAt(random.nextInt(26));
-            sessionKey+=c;
-        }
-        User newMember = UserRepository.existingUser(newUser.Email,
-                hashedPassword, sessionKey);
-        if(newMember != null){
+    @CrossOrigin
+    @PostMapping("/login")
+    public User logIn(@RequestBody LogIn currentUser) {
+        System.out.println(currentUser.email);
+        System.out.println(currentUser.password);
+        String hashedPassword = BCrypt.hashpw(currentUser.password, salt);
+        String sessionKey = new SignUpController().createSessionKey();
+        User newMember = UserRepository.existingUser(currentUser.email, hashedPassword, sessionKey);
+        System.out.println(currentUser.email);
+        System.out.println(hashedPassword);
+        System.out.println(sessionKey);
+        if (newMember != null) {
             return newMember;
         } else {
-            System.out.println("There was an error");
+            System.out.println("Something went Wrong!");
             return null;
         }
     }
