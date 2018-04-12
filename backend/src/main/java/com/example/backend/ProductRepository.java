860d52113f57a52033adf17ecd946235b1e95e85
Osvaldo Quinonez <oquinonez@basecampcodingacademy.org>
2018-04-12 09:20:31 -0500

    Added prints so i could see what was being passed around

--- backend/src/main/java/com/example/backend/ProductRepository.java
+++ backend/src/main/java/com/example/backend/ProductRepository.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class ProductRepository {
     public static ArrayList<Product> allProducts() {
@@ -12,18 +13,25 @@ public class ProductRepository {
             Connection con = Connect.LoadDB();
             PreparedStatement statement = con.prepareStatement("SELECT * FROM Products");
             ResultSet resultSet = statement.executeQuery();
+            System.out.println(resultSet);
             ArrayList<Product> items = new ArrayList<Product>();
             while (resultSet.next()) {
-                allProducts().add(new Product(resultSet.getString("ItemName"),
+                items.add(new Product(
+                        resultSet.getString("ItemName"),
                         resultSet.getString("Category"),
                         resultSet.getDouble("Price"),
                         resultSet.getInt("Quantity"),
-                        resultSet.getString("PicAddress")));
+                        resultSet.getString("PicAddress")
+                ));
             }
-            con.close();
-            System.out.println(allProducts().get(1).ItemName);
-            return allProducts();
+//            con.close();
+            System.out.println(items);
+            System.out.println("It some how works");
+//            System.out.println((items.get(1).ItemName));
+            return items;
         } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("There was an error somewhere...");
             return null;
         }
     }
